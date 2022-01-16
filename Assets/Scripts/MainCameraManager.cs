670e9e4e5e101a03706b16267838d1a5f94d2783
Richard Norbron <norbstarone@gmail.com>
2022-01-16 02:21:16 +0000

    Fixed the issue of calculating and enforcing near/far distance focusing on grabbable objects.

--- Assets/Scripts/MainCameraManager.cs
+++ Assets/Scripts/MainCameraManager.cs
@@ -21,6 +21,7 @@ public class MainCameraManager : Gizmo
     [SerializeField] AudioClip undockClip;
 
     [Header("Focus")]
+    [SerializeField] float focalRadius = 0.5f;
     [SerializeField] float nearDistance;
     [SerializeField] float farDistance;
 
@@ -152,12 +153,81 @@ public class MainCameraManager : Gizmo
 
     void FixedUpdate()
     {
-        Debug.DrawRay(transform.TransformPoint(Vector3.zero), transform.forward, Color.red);
+        // Debug.DrawRay(transform.TransformPoint(Vector3.zero), transform.forward, Color.white);
 
-        if (Physics.SphereCast(transform.TransformPoint(Vector3.zero), 0.25f, transform.forward, out RaycastHit hit, Mathf.Infinity, defaultLayerMask))
+        bool hitDetected = Physics.SphereCast(transform.TransformPoint(Vector3.zero), focalRadius, transform.forward, out RaycastHit hitInfo, Mathf.Infinity, defaultLayerMask);
+        GameObject objectHit = null;
+        Vector3 point = default(Vector3);
+        bool isValid = false;
+
+        Debug.Log($"HitDetected:{hitDetected}");
+
+        if (hitDetected)
         {
-            var objectHit = hit.transform.gameObject;
-            var point = hit.point;
+            objectHit = hitInfo.transform.gameObject;
+            point = hitInfo.point;
+
+            var distanceToPoint = Vector3.Distance(transform.position, point);
+            Debug.Log($"DistanceToPoint:{distanceToPoint}");
+
+            if ((distanceToPoint <= farDistance) && (distanceToPoint >= nearDistance))
+            {
+                isValid = true;
+            }
+        }
+
+        Debug.Log($"IsValid:{isValid}");
+
+        if (isValid)
+        {
+
+            if (showHits)
+            {
+                if (hitPrefabInstance == null)
+                {
+                    hitPrefabInstance = Instantiate(hitPrefab, point, objectHit.transform.rotation);
+                }
+                else
+                {
+                    hitPrefabInstance.transform.position = point;
+                    hitPrefabInstance.SetActive(true);
+                }
+            }
+
+            if (!GameObject.ReferenceEquals(objectHit, lastObjectHit))
+            {
+                if (lastFocus != null)
+                {
+                    lastFocus.LostFocus(gameObject);
+                    lastFocus = null;
+                }
+
+                if (objectHit.TryGetComponent<IFocus>(out IFocus focus))
+                {
+                    focus.GainedFocus(gameObject);
+                    lastFocus = focus;
+                }
+
+                lastObjectHit = objectHit;
+            }
+        }
+        else
+        {
+            if (lastFocus != null)
+            {
+                lastFocus.LostFocus(gameObject);
+                lastFocus = null;
+            }
+
+            hitPrefabInstance?.SetActive(false);
+            lastObjectHit = null;
+        }
+
+#if false
+        if (Physics.SphereCast(transform.TransformPoint(Vector3.zero), focalRadius, transform.forward, out RaycastHit hitInfo, Mathf.Infinity, defaultLayerMask))
+        {
+            var objectHit = hitInfo.transform.gameObject;
+            var point = hitInfo.point;
 
             if (showHits)
             {
@@ -208,6 +278,7 @@ public class MainCameraManager : Gizmo
             hitPrefabInstance?.SetActive(false);
             lastObjectHit = null;
         }
+#endif
     }
 
     public bool TryGetFocus(out GameObject focus)
---
+++ Assets/Scripts/SphereCastRayCameraController.cs
@@ -0,0 +1,150 @@
+using UnityEngine;
+
+public class SphereCastRayCameraController : Gizmo
+{
+    [Header("Hits")]
+    [SerializeField] bool showHits;
+    [SerializeField] GameObject hitPrefab;
+
+    [Header("Focus")]
+    [SerializeField] float focalRadius = 0.5f;
+    [SerializeField] float nearDistance;
+    [SerializeField] float farDistance;
+
+    private GameObject hitPrefabInstance;
+    private GameObject lastObjectHit;
+    private IFocus lastFocus;
+    private int defaultLayerMask;
+
+    void Awake()
+    {
+        defaultLayerMask = LayerMask.GetMask("Default");
+    }
+
+    void FixedUpdate()
+    {
+        // Debug.DrawRay(transform.TransformPoint(Vector3.zero), transform.forward, Color.white);
+
+        bool hitDetected = Physics.SphereCast(transform.TransformPoint(Vector3.zero), focalRadius, transform.forward, out RaycastHit hitInfo, Mathf.Infinity, defaultLayerMask);
+        GameObject objectHit = null;
+        Vector3 point = default(Vector3);
+        bool isValid = false;
+
+        Debug.Log($"HitDetected:{hitDetected}");
+
+        if (hitDetected)
+        {
+            objectHit = hitInfo.transform.gameObject;
+            point = hitInfo.point;
+
+            var distanceToPoint = Vector3.Distance(transform.position, point);
+            Debug.Log($"DistanceToPoint:{distanceToPoint}");
+
+            if ((distanceToPoint <= farDistance) && (distanceToPoint >= nearDistance))
+            {
+                isValid = true;
+            }
+        }
+
+        Debug.Log($"IsValid:{isValid}");
+
+        if (isValid)
+        {
+
+            if (showHits)
+            {
+                if (hitPrefabInstance == null)
+                {
+                    hitPrefabInstance = Instantiate(hitPrefab, point, objectHit.transform.rotation);
+                }
+                else
+                {
+                    hitPrefabInstance.transform.position = point;
+                    hitPrefabInstance.SetActive(true);
+                }
+            }
+
+            if (!GameObject.ReferenceEquals(objectHit, lastObjectHit))
+            {
+                if (lastFocus != null)
+                {
+                    lastFocus.LostFocus(gameObject);
+                    lastFocus = null;
+                }
+
+                if (objectHit.TryGetComponent<IFocus>(out IFocus focus))
+                {
+                    focus.GainedFocus(gameObject);
+                    lastFocus = focus;
+                }
+
+                lastObjectHit = objectHit;
+            }
+        }
+        else
+        {
+            if (lastFocus != null)
+            {
+                lastFocus.LostFocus(gameObject);
+                lastFocus = null;
+            }
+
+            hitPrefabInstance?.SetActive(false);
+            lastObjectHit = null;
+        }
+
+#if false
+        if (Physics.SphereCast(transform.TransformPoint(Vector3.zero), focalRadius, transform.forward, out RaycastHit hitInfo, Mathf.Infinity, defaultLayerMask))
+        {
+            var objectHit = hitInfo.transform.gameObject;
+            var point = hitInfo.point;
+
+            if (showHits)
+            {
+                if (hitPrefabInstance == null)
+                {
+                    hitPrefabInstance = Instantiate(hitPrefab, point, objectHit.transform.rotation);
+                }
+                else
+                {
+                    hitPrefabInstance.transform.position = point;
+                    hitPrefabInstance.SetActive(true);
+                }
+            }
+
+            if (!GameObject.ReferenceEquals(objectHit, lastObjectHit))
+            {
+                if (lastFocus != null)
+                {
+                    lastFocus.LostFocus(gameObject);
+                    lastFocus = null;
+                }
+
+                if (objectHit.TryGetComponent<IFocus>(out IFocus focus))
+                {
+                    var distanceToPoint = Vector3.Distance(transform.position, point);
+
+                    if ((distanceToPoint <= farDistance) && (distanceToPoint >= nearDistance))
+                    {
+                        focus.GainedFocus(gameObject);
+                        lastFocus = focus;
+                    }
+                }
+
+                lastObjectHit = objectHit;
+            }
+        }
+        else
+        {
+            if (lastFocus != null)
+            {
+                lastFocus.LostFocus(gameObject);
+                lastFocus = null;
+            }
+
+            hitPrefabInstance?.SetActive(false);
+            lastObjectHit = null;
+        }
+#endif
+    }
+}
