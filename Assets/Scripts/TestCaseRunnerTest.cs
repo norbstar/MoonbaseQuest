69ee26ee4b5ff569ff5b51cf82c246984162d2e9
Richard Norbron <norbstarone@gmail.com>
2022-01-18 13:13:02 +0000

    Split test case runner test into two parts; the data source and the event receiver.

--- Assets/Scripts/TestCaseRunnerTest.cs
+++ Assets/Scripts/TestCaseRunnerTest.cs
@@ -24,45 +24,15 @@ public class TestCaseRunnerTest : MonoBehaviour
 
     private IEnumerator PostSequenceCoroutine()
     {
-        TestCaseRunner.EventReceived += OnEvent;
-        
-        testCaseRunner.SetExpectedSequence(expectedSequence);
-        
-        foreach (string item in postedSequence)
+        if (testCaseRunner != null)
         {
-            yield return new WaitForSeconds(1f);
-            testCaseRunner.Post(item);
-        }
-    }
-
-    public void OnEvent(TestCaseRunner.State state, object data)
-    {
-        switch (state)
-        {
-            case TestCaseRunner.State.Start:
-                Debug.Log($"{Time.time} Start");
-                break;
-
-            case TestCaseRunner.State.Pass:
-                Debug.Log($"{Time.time} Pass");
-                break;
-
-            case TestCaseRunner.State.Fail:
-                Debug.Log($"{Time.time} Fail");
-
-                List<TestCaseRunner.DataPoint> dataPoints = (List<TestCaseRunner.DataPoint>) data;
-                
-                for (int idx = 0; idx < dataPoints.Count; idx++)
-                {
-                    TestCaseRunner.DataPoint dataPoint = dataPoints[idx];
-                    Debug.Log($"{Time.time} DataPoint[{idx}] Expected : {dataPoint.expected} Posted : {dataPoint.posted} Pass : {dataPoint.pass}");
-                }
-                
-                break;
-
-            case TestCaseRunner.State.End:
-                Debug.Log($"{Time.time} End");
-                break;
+            testCaseRunner.SetExpectedSequence(expectedSequence);
+            
+            foreach (string item in postedSequence)
+            {
+                yield return new WaitForSeconds(1f);
+                testCaseRunner.Post(item);
+            }
         }
     }
 }
