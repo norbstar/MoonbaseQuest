c2c696eb564959fb79a286a36a153ca7f29008c2
Richard Norbron <norbstarone@gmail.com>
2022-04-12 21:47:13 +0100

    Added robot Kyle into the scene with a simple repeat animation to give the scene an observer.

--- Assets/Scripts/Chess/Pieces/RookManager.cs
+++ Assets/Scripts/Chess/Pieces/RookManager.cs
@@ -6,7 +6,81 @@ namespace Chess.Pieces
     {
         protected override List<Cell> ResolveAllAvailableQualifyingCells(Cell[,] matrix)
         {
-            return new List<Cell>();
+            List<Cell> cells = new List<Cell>();
+            List<Coord> coords = GenerateCoords(matrix);
+
+            // TODO
+
+            return cells;
+        }
+
+        private List<Coord> GenerateCoords(Cell[,] matrix)
+        {
+            List<Coord> coords = new List<Coord>();
+
+            Coord activeCoord = ActiveCell.coord;
+            Coord coord;
+            
+            coord = activeCoord;
+
+            while (coord.x >= 0)
+            {
+                --coord.x;
+
+                coords.Add(new Coord
+                {
+                    x = coord.x,
+                    y = coord.y
+                });
+            }
+
+            coord = activeCoord;
+
+            while (coord.x <= (maxColumnIdx - 1))
+            {
+                ++coord.x;
+                
+                coords.Add(new Coord
+                {
+                    x = coord.x,
+                    y = coord.y
+                });
+            }
+
+            coord = activeCoord;
+
+            while (coord.y >= 0)
+            {
+                --coord.y;
+                
+                coords.Add(new Coord
+                {
+                    x = coord.x,
+                    y = coord.y
+                });
+            }
+
+            coord = activeCoord;
+
+            while (coord.y <= (maxRowIdx - 1))
+            {
+                ++coord.y;
+                
+                coords.Add(new Coord
+                {
+                    x = coord.x,
+                    y = coord.y
+                });
+            }
+
+            return coords;
+        }
+    
+        private bool HasLineOfSightToCoord(Coord coord)
+        {
+            // TODO
+            
+            return false;
         }
     }
 }
